8148e8372e7376c1c304986695ef8c49a387c9eb
Kover24 <klich27150@student.pwsztar.edu.pl>
2017-05-24 11:08:47 +0200

    Code to preview

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,8 +3,14 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
-describe('getDescendingNumbers', function (){
+describe('getDescendingNumbers', function ()
     it('should return string with numbers separated by spaces', function ()    {
         expect(app.getDescendingNumbers(15, 1)).to.eql('15 14 13 12 11 10 9 8 7 6 5 4 3 2 1');
     });
 });
+var x = 5
+var y = 0
+while (y < x){
+y+
+    Console.Log("Sample text")
+}
